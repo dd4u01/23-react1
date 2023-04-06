c38ce068e6f469ba1fe7fd2c0b92b413cce6c398
dd4u01 <dd4u01@gmail.com>
2023-04-06 15:34:52 +0900

    0406 temp2

--- src/chapter_05/CommentList.jsx
+++ src/chapter_05/CommentList.jsx
@@ -1,10 +1,29 @@
 import React from "react";
 import Comment from "./Comment";
 
+const comments = [
+    {
+        name: "유재석",
+        comment: "안녕하세요, 유재석입니다.",
+    },
+    {
+        name: "박명수",
+        comment: "리액트 재미있어요~!",
+    },
+    {
+        name: "정준하",
+        comment: "저도 리액트 배워보고 싶어요!",
+    },
+];
+
 function CommentList(props) {
     return(
         <div>
-            <Comment />
+            {comments.map((comment) => {
+                return(
+                    <Comment name={comment.name} comment={comment.comment} />
+                );
+            })}
         </div>
     );
 }
