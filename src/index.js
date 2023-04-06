b73414f196ff136c544a45d169dd69e27cb2b7be
dd4u01 <dd4u01@gmail.com>
2023-04-06 18:29:53 +0900

    0406 final

--- src/index.js
+++ src/index.js
@@ -3,13 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Clock from './chapter_04/Clock';
-import CommentList from './chapter_05/CommentList';
+import NotificationList from './chchch/NotificationList';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <CommentList/>
+    <NotificationList/>
   </React.StrictMode>,
 );
 
