4f8d068ba9f75618e643a2c7ecd3b4d8828ca224
dd4u01 <dd4u01@gmail.com>
2023-04-13 15:47:41 +0900

    0413 CH07

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import NotificationList from './chchch/NotificationList';
+import Accommodate from './chapter_07/Accommodate';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <NotificationList/>
+    <Accommodate/>
   </React.StrictMode>,
 );
 
